76fac50240a65dbfdde1d441c516c1a5fd9f8c42
Aaron J. Rhodes <aaronj.rhodes@Aarons-MBP.attlocal.net>
2025-01-03 12:58:35 -0800

    removed unused imports

--- MastermindGame/src/main/java/com/pluralsight/RandomNumberGenerator.java
+++ MastermindGame/src/main/java/com/pluralsight/RandomNumberGenerator.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 public class RandomNumberGenerator {
     public int[] generateRandomNumbers(int numDigits) {
